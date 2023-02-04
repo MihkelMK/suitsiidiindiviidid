1f2bfa04e2d458b0103a71faeb838bd352a5931d
Richard Jõgi <richar02@ut.ee>
2023-02-04 21:23:05 +0200

    Added valvur and movement nodes

---
+++ src/main/Valvur.gd
@@ -0,0 +1,55 @@
+extends KinematicBody2D
+
+
+export var patrol = false
+export var direction = true
+export var walkrange = 200
+
+var wait = false
+var origin = position.x
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	origin = position.x
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not wait:
+		if direction:
+			move_and_collide(Vector2(1,0))
+		#print("parem")
+		if not direction:
+			move_and_collide(Vector2(-1,0))
+		#print("vasak")
+
+# Deprecated
+func _patrol():
+	print(origin)
+	print(position.x)
+	if (origin >= position.x or position.x >= origin+walkrange):
+		flip()
+		
+	if direction:
+		move_and_collide(Vector2(1,0))
+		#print("parem")
+		
+	if not direction:
+		move_and_collide(Vector2(-1,0))
+		#print("vasak")
+		
+func flip():
+		direction = not direction
+		scale.x *= -1
+		print("Flip!")
+		
+func wait(time):
+	wait = true
+	$WaitTime.start(time)
+
+func _on_ViewArea_body_entered(body):
+	print("KUULE!")
+
+
+func _on_WaitTime_timeout():
+	wait = false
