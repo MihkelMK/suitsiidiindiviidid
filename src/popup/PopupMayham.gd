f81d9833f40dc35ea09de7ab253f37b9feab61e4
Mihkel Martin Kasterpalu <43957228+MihkelMK@users.noreply.github.com>
2023-02-04 19:00:22 +0200

    fixed click

--- src/popup/PopupMayham.gd
+++ src/popup/PopupMayham.gd
@@ -5,7 +5,7 @@ export(PackedScene) var hack_popup_scene
 
 var popupCount
 var winstate = 0
-var pregeneratedPopups = 5
+var pregeneratedPopups = 8
 
 onready var screenSize = $Screen/Sprite.get_rect().size
 onready var viewSize = get_viewport_rect().size
@@ -42,8 +42,7 @@ func createPopup():
 	var popup = popup_scene.instance()
 
 	# Set the popup's position to a random location.
-#	popup.position = Vector2(viewSize.x/2 + screenSize.x*rand_range(-1,1), viewSize.y/2 + screenSize.y*rand_range(-1.4,1))
-	popup.position = Vector2(viewSize.x/2, viewSize.y/2)
+	popup.position = Vector2(viewSize.x/2 + screenSize.x*rand_range(-1,1), viewSize.y/2 + screenSize.y*rand_range(-1.4,1))
 
 	# Spawn the popup by adding it to the Main scene.
 	add_child(popup)
@@ -57,3 +56,16 @@ func createHackPopup():
 
 	# Spawn the popup by adding it to the Main scene.
 	add_child(popup)
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
+		var objects_clicked = get_world_2d().direct_space_state.intersect_point(event.position, 32, [], 0x7FFFFFFF, true, true )
+		if objects_clicked.size() > 0:
+			var clicked = get_topmost_object(objects_clicked)
+			if !("Screen" in clicked.name): clicked.queue_free()
+
+func get_topmost_object(objects_list):
+	for i in range(0, objects_list.size()):
+		objects_list[i] = objects_list[i].collider.get_parent().get_position_in_parent()
+
+	return get_child(objects_list.max())
